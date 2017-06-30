172bdccd6433142c1683131e7778029899ff46f0
Subhan Ahmed <subhanahmed047@gmail.com>
2017-06-30 10:44:22 +1000

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { UserAddComponent } from './users/user-add/user-add.component';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { PostAddComponent } from './posts/post-add/post-add.component';
+import { PostEditComponent } from './posts/post-edit/post-edit.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { ProfileComponent } from './users/profile/profile.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: PostsListComponent},
+  {path: 'posts/add', component: PostAddComponent},
+  {path: 'posts/edit/:id', component: PostEditComponent},
+  {path: 'posts/view/:id', component: SinglePostComponent},
+  {path: 'users/add', component: UserAddComponent},
+  {path: 'users/edit/:id', component: UserEditComponent},
+  {path: 'user/profile/:id', component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserAddComponent,
+    UserEditComponent,
+    PostsListComponent,
+    PostAddComponent,
+    PostEditComponent,
+    SinglePostComponent,
+    ProfileComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+      //{enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
